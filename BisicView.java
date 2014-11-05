6f17bcc5d159623310bd9548079f7af0b22c3cd9
Logdanidis <xarislog@gmail.com>
2014-11-05 12:30:13 +0200

    Update BisicView.java

--- BisicView.java
+++ BisicView.java
@@ -30,5 +30,26 @@ public class BasicView implements Serializable{
  
     public void increment() {
         number++;
+        saveMessage();
+	/** kalei tin saveMessage gia na doulepsei to growl */
     }
+    /**
+	 * growl msg
+	 * 
+	 * private String message;
+	 * 
+	 * public String getMessage() { return message; }
+	 */
+	public void setMessage(String message) {
+		this.text = message;
+	}
+
+	public void saveMessage() {
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		context.addMessage(null, new FacesMessage("Successful",
+				"Your message: " + text));
+		context.addMessage(null, new FacesMessage("Second Message",
+				"Additional Message Detail"));
+	}
 }
